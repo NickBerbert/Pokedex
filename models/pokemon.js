f40961f9f70c315aefbbb3e531df0ceb2c8d8310
NickBerbert <140767582+NickBerbert@users.noreply.github.com>
2024-10-11 20:22:22 -0300

    Add files via upload

---
+++ models/pokemon.js
@@ -0,0 +1,10 @@
+class pokemon{
+    constructor(nome, tipo, altura, peso, poder){
+        const nome = nome;
+        const tipo = tipo; 
+        const altura = altura;
+        const peso = peso;
+        const poder = poder;
+
+    }
+}
