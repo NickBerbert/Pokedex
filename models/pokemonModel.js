5bb4195ac93c4fa8a640d10310734af41478f8c2
NicolasBerbert <nicolasberbert@edu.unifil.br>
2024-10-17 19:47:31 -0300

    aa

---
+++ models/pokemonModel.js
@@ -0,0 +1,12 @@
+
+
+class pokemon{
+    constructor(nome, tipo, altura, peso, poder){
+        this.nome = nome;
+        this.tipo = tipo; 
+        this.altura = altura;
+        this.peso = peso;
+        this.poder = poder;
+    }
+}
+
